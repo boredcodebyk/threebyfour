34010d10b271760bd31a2cccb2e69148174c6691
Kaushik <25322315+boredcodebyk@users.noreply.github.com>
2025-08-03 14:40:56 +0530

    add transition and bg color to show.dart

--- lib/view/show.dart
+++ lib/view/show.dart
@@ -7,19 +7,35 @@ import 'package:photo_view/photo_view_gallery.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class ShowView extends ConsumerStatefulWidget {
-  const ShowView({super.key, required this.list});
+  ShowView({super.key, required this.list, this.initialIndex = 0})
+    : pageController = PageController(initialPage: initialIndex);
   final List<FileSystemEntity> list;
+  final int initialIndex;
+  final PageController pageController;
   @override
   ConsumerState<ShowView> createState() => _ShowViewState();
 }
 
 class _ShowViewState extends ConsumerState<ShowView> {
+  late int currentIndex = widget.initialIndex;
+
+  void onPageChanged(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: PhotoViewGallery.builder(
         itemCount: widget.list.length,
+        pageController: widget.pageController,
+        onPageChanged: onPageChanged,
+        backgroundDecoration: BoxDecoration(
+          color: Theme.of(context).scaffoldBackgroundColor
+        ),
         scrollPhysics: const BouncingScrollPhysics(),
         loadingBuilder: (context, event) => Image.memory(kTransparentImage),
         builder: (context, index) {
@@ -27,8 +43,8 @@ class _ShowViewState extends ConsumerState<ShowView> {
           return PhotoViewGalleryPageOptions(
             imageProvider: FileImage(item as File),
             filterQuality: FilterQuality.high,
-            minScale: PhotoViewComputedScale.contained * (0.5 + index / 10),
-            initialScale: PhotoViewComputedScale.contained,
+            minScale: PhotoViewComputedScale.contained * (0.4),
+            initialScale: PhotoViewComputedScale.contained * (0.8),
           );
         },
       ),
