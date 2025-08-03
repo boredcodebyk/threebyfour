255158b9408da0a3a8ed94f90c18ad595707616e
Kaushik <25322315+boredcodebyk@users.noreply.github.com>
2025-08-03 10:09:50 +0530

    fix grid display

--- lib/view/gallery.dart
+++ lib/view/gallery.dart
@@ -45,184 +45,184 @@ class _GalleryViewState extends ConsumerState<GalleryView>
     refreshImages();
     super.build(context);
     return Scaffold(
+      resizeToAvoidBottomInset: true,
+      extendBody:true,
       appBar: AppBar(),
-      
-
       body: Stack(
         // crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Expanded(
-            child: MasonryGridView.count(
-              crossAxisCount:
-                  ResponsiveBreakpoints.of(context).isDesktop
-                      ? 4
-                      : ResponsiveBreakpoints.of(context).isTablet
-                      ? 3
-                      : 2,
-              crossAxisSpacing: 1,
-              mainAxisSpacing: 1,
-              shrinkWrap: true,
-              itemCount: imageList.length,
-              itemBuilder: (BuildContext context, int index) {
-                var imageFile = imageList[index];
-                return Card(
-                  elevation: 0,
-                  clipBehavior: Clip.antiAlias,
-                  child: FadeInImage(
-                    placeholder: MemoryImage(kTransparentImage),
+          MasonryGridView.count(
+            crossAxisCount:
+                ResponsiveBreakpoints.of(context).isDesktop
+                    ? 4
+                    : ResponsiveBreakpoints.of(context).isTablet
+                    ? 3
+                    : 2,
+            crossAxisSpacing: 1,
+            mainAxisSpacing: 1,
+            shrinkWrap: true,
+            itemCount: imageList.length,
+            itemBuilder: (BuildContext context, int index) {
+              var imageFile = imageList[index];
+              return Card(
+                elevation: 0,
+                clipBehavior: Clip.antiAlias,
+                child: FadeInImage(
+                  placeholder: MemoryImage(kTransparentImage),
 
-                    image: FileImage(imageFile as File),
-                    filterQuality: FilterQuality.low,
-                  ),
-                );
-              },
-            ),
+                  image: FileImage(imageFile as File),
+                  filterQuality: FilterQuality.low,
+                ),
+              );
+            },
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-                    padding: const EdgeInsets.only(bottom: 16),
-                    child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: Card(
-                  elevation: 6,
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(99)),
-                  ),
-                  child: SizedBox(
-                    height: 64,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8),
+          SafeArea(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
                       child: Row(
-                        spacing: 4,
-                        mainAxisSize: MainAxisSize.max,
-                        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          IconButton(
-                            icon: Icon(Icons.casino_outlined),
-                            onPressed: () {},
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.arrow_downward),
-                            onPressed: () {},
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.arrow_left),
-                            onPressed: () {},
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.slideshow),
-                            onPressed: () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(builder: (context) => ShowView(list: imageList),)
-                              );
-                            },
-                          ),
-                        ],
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Card(
+                    elevation: 6,
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(99)),
+                    ),
+                    child: SizedBox(
+                      height: 64,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                        child: Row(
+                          spacing: 4,
+                          mainAxisSize: MainAxisSize.max,
+                          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.casino_outlined),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.arrow_downward),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.arrow_left),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.slideshow),
+                              onPressed: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(builder: (context) => ShowView(list: imageList),)
+                                );
+                              },
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            
-              FloatingActionButton(
-                onPressed:
-                    () => showModalBottomSheet(
-                      context: context,
-                      builder: (context) {
-                        return StatefulBuilder(
-                          builder: (context, StateSetter setState) {
-                            return SizedBox(
-                              height: 360,
-                              child: Column(
-                                children: [
-                                  ListTile(
-                                    leading: Icon(Icons.timer_outlined),
-                                    title: Text("Display each image for"),
-                                    subtitle: Wrap(
-                                      spacing: 2,
-                                      children: [
-                                        FilterChip(
-                                          label: Text("30s"),
-                                          selected: timerIndex == 0,
-                                          onSelected:
-                                              (value) => setState(() {
-                                                timerIndex = 0;
-                                              }),
-                                        ),
-                                        FilterChip(
-                                          label: Text("60s"),
-                                          selected: timerIndex == 1,
-                                          onSelected:
-                                              (value) => setState(() {
-                                                timerIndex = 1;
-                                              }),
-                                        ),
-                                        FilterChip(
-                                          label: Text("2 mins"),
-                                          selected: timerIndex == 2,
-                                          onSelected:
-                                              (value) => setState(() {
-                                                timerIndex = 2;
-                                              }),
-                                        ),
-                                        FilterChip(
-                                          label: Text("5 mins"),
-                                          selected: timerIndex == 3,
-                                          onSelected:
-                                              (value) => setState(() {
-                                                timerIndex = 3;
-                                              }),
-                                        ),
-                                        FilterChip(
-                                          label: Text("10 mins"),
-                                          selected: timerIndex == 4,
-                                          onSelected:
-                                              (value) => setState(() {
-                                                timerIndex = 4;
-                                              }),
-                                        ),
-                                      ],
+
+                FloatingActionButton(
+                  onPressed:
+                      () => showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return StatefulBuilder(
+                            builder: (context, StateSetter setState) {
+                              return SizedBox(
+                                height: 360,
+                                child: Column(
+                                  children: [
+                                    ListTile(
+                                      leading: Icon(Icons.timer_outlined),
+                                      title: Text("Display each image for"),
+                                      subtitle: Wrap(
+                                        spacing: 2,
+                                        children: [
+                                          FilterChip(
+                                            label: Text("30s"),
+                                            selected: timerIndex == 0,
+                                            onSelected:
+                                                (value) => setState(() {
+                                                  timerIndex = 0;
+                                                }),
+                                          ),
+                                          FilterChip(
+                                            label: Text("60s"),
+                                            selected: timerIndex == 1,
+                                            onSelected:
+                                                (value) => setState(() {
+                                                  timerIndex = 1;
+                                                }),
+                                          ),
+                                          FilterChip(
+                                            label: Text("2 mins"),
+                                            selected: timerIndex == 2,
+                                            onSelected:
+                                                (value) => setState(() {
+                                                  timerIndex = 2;
+                                                }),
+                                          ),
+                                          FilterChip(
+                                            label: Text("5 mins"),
+                                            selected: timerIndex == 3,
+                                            onSelected:
+                                                (value) => setState(() {
+                                                  timerIndex = 3;
+                                                }),
+                                          ),
+                                          FilterChip(
+                                            label: Text("10 mins"),
+                                            selected: timerIndex == 4,
+                                            onSelected:
+                                                (value) => setState(() {
+                                                  timerIndex = 4;
+                                                }),
+                                          ),
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                  // ListTile(
-                                  //   leading: Icon(Icons.image),
-                                  //   title: Text("Number of images in practice"),
-                                  // ),
-                                  // ListTile(
-                                  //   leading: Icon(Icons.all_inclusive),
-                                  //   title: Text("Unlimited"),
-                                  //   trailing: Radio(value: "unlimited",
-                                  //   groupValue: "limit",
-                                  //   onChanged: (value) {},),
-                                  // ),
-                                  // ListTile(
-                                  //   leading: Icon(Icons.tag),
-                                  //   title: Text("limited"),
-                                  //   trailing: Radio(value: "unlimited",
-                                  //   groupValue: "limit",
-                                  //   onChanged: (value) {},),
-                                  // ),
-                                  FilledButtonLarge(label: "Start", onPressed: (){})
-                                ],
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    ),
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                child: Icon(Icons.timer_outlined),
+                                    // ListTile(
+                                    //   leading: Icon(Icons.image),
+                                    //   title: Text("Number of images in practice"),
+                                    // ),
+                                    // ListTile(
+                                    //   leading: Icon(Icons.all_inclusive),
+                                    //   title: Text("Unlimited"),
+                                    //   trailing: Radio(value: "unlimited",
+                                    //   groupValue: "limit",
+                                    //   onChanged: (value) {},),
+                                    // ),
+                                    // ListTile(
+                                    //   leading: Icon(Icons.tag),
+                                    //   title: Text("limited"),
+                                    //   trailing: Radio(value: "unlimited",
+                                    //   groupValue: "limit",
+                                    //   onChanged: (value) {},),
+                                    // ),
+                                    FilledButtonLarge(label: "Start", onPressed: (){})
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                  child: Icon(Icons.timer_outlined),
+                ),
+                ],
               ),
-            ],
-                    ),
-                  ),
+              ),
+            ),
           ),
         ],
       ),
